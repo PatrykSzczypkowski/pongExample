c856e5a09a02dbf90005e3c03b005349f470d677
Patryk <patszc@icloud.com>
2020-10-04 22:23:35 +0100

    Added goals and score tracking

---
+++ Assets/Ball.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "RacketLeft")
+        {
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "RacketRight")
+        {
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "GoalLeft")
+        {
+            GameObject.Find("ScoreRight").GetComponent<Score>().addScoreRight(); 
+            transform.position = new Vector2(0, 0);
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        }
+
+        if (col.gameObject.name == "GoalRight")
+        {
+            GameObject.Find("ScoreLeft").GetComponent<Score>().addScoreLeft(); 
+            transform.position = new Vector2(0, 0);
+            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static int scoreLeft;
+    public static int scoreRight;
+
+    public void addScoreLeft()
+    {
+        scoreLeft += 1;
+        transform.GetChild(scoreLeft - 1).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+    }
+
+    public void addScoreRight()
+    {
+        scoreRight += 1;
+        transform.GetChild(scoreRight - 1).GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreLeft = 0;
+        scoreRight = 0;
+        foreach (Transform child in transform)
+            child.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
